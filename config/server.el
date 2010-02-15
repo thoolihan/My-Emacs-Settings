14783d207f4f0f6a12ffd7e31edf7d7125548f65
unknown <tim@.(none)>
2010-02-15 14:20:15 -0500

    server clients (emacsclientw) no longer open a new frame

--- config/server.el
+++ config/server.el
@@ -1,8 +1,8 @@
-(add-hook 'server-switch-hook
-          (lambda nil
-            (let ((server-buf (current-buffer)))
-              (bury-buffer)
-              (switch-to-buffer-other-frame server-buf))))
-
+;(add-hook 'server-switch-hook
+;          (lambda nil
+;            (let ((server-buf (current-buffer)))
+;              (bury-buffer)
+;              (switch-to-buffer-other-frame server-buf))))
+;
 (custom-set-variables '(server-kill-new-buffers t))
-(add-hook 'server-done-hook (lambda () (delete-frame)))
+;(add-hook 'server-done-hook (lambda () (delete-frame)))

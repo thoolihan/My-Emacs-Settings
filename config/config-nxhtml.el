25295e85877c68df33f6923e847dd0d03a36f363
unknown <tim@.(none)>
2010-04-13 17:53:20 -0400

    added nxhtml

---
+++ config/config-nxhtml.el
@@ -0,0 +1,1 @@
+(load-file "~/emacs/nxhtml/autostart.el")
--- config/config-nxml.el
+++
@@ -1,7 +0,0 @@
-(add-path "nxml/")
-
-(load-library "rng-auto.el")
-(setq auto-mode-alist
-      (cons '("\\.\\(xml\\|xaml\\|aspx\\|xsl\\|rng\\|xhtml\\)\\'" . nxml-mode)
-            auto-mode-alist))
-(setq nxml-slash-auto-complete-flag t)

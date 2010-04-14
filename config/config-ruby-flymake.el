4616e089a42661c41411637058c6e8f1ecd52959
tim <tim@ubuntu-server32.safe-prop.com>
2010-04-13 23:17:08 -0400

    flymake working for ruby

--- config/config-ruby-flymake.el
+++ config/config-ruby-flymake.el
@@ -1,21 +1,27 @@
-(defvar flymake-ruby-err-line-patterns '(("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3)))
-(defvar flymake-ruby-allowed-file-name-masks '((".+\\.\\(rb\\|rake\\)$" flymake-ruby-init)
-                                               ("Rakefile$" flymake-ruby-init)))
+(require 'flymake)
 
-;; Not provided by flymake itself, curiously
-(defun flymake-create-temp-in-system-tempdir (filename prefix)
-  (make-temp-file (or prefix "flymake-ruby")))
+;; I don't like the default colors :)
+(set-face-background 'flymake-errline "red4")
+(set-face-background 'flymake-warnline "dark slate blue")
 
 ;; Invoke ruby with '-c' to get syntax checking
 (defun flymake-ruby-init ()
-  (list "ruby" (list "-c" (flymake-init-create-temp-buffer-copy
-                           'flymake-create-temp-in-system-tempdir))))
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+          (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "ruby" (list "-c" local-file))))
 
-(defun flymake-ruby-load ()
-  (interactive)
-  (set (make-local-variable 'flymake-allowed-file-name-masks) flymake-ruby-allowed-file-name-masks)
-  (set (make-local-variable 'flymake-err-line-patterns) flymake-ruby-err-line-patterns)
-  (flymake-mode t))
+(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
 
+(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
 
-(provide 'flymake-ruby)
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+
+                  ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
+                  (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
+                       (flymake-mode))
+                       ))

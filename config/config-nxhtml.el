edac5db97d0f8d0a7257afceeeec3aae55047aef
Tim Hoolihan <Tim@Hoolihan.net>
2010-06-17 15:00:41 -0400

    .xaml and .config now use nxhtml

--- config/config-nxhtml.el
+++ config/config-nxhtml.el
@@ -1,1 +1,7 @@
 (load-file "~/emacs/nxhtml/autostart.el")
+
+(setq auto-mode-alist
+      (append '(("\\.xaml$" . nxhtml-mode)) auto-mode-alist))
+
+(setq auto-mode-alist
+      (append '(("\\.config$" . nxhtml-mode)) auto-mode-alist))

e300e093c4a7022ce0af0e0247284a0e6e61229e
Tim Hoolihan <Tim@Hoolihan.net>
2010-12-30 09:03:35 -0500

    added params

--- misc/ext-tools.el
+++ misc/ext-tools.el
@@ -2,32 +2,37 @@
   "launch an external tool async"
   (shell-command (concat path " &")))
 
-(defun ext-tools-start-visual-studio
+(defun ext-tools-start-visual-studio ()
   "launch visual studio"
   (interactive
    (ext-tools-launch ext-tools-path-ide-net)))
 
-(defun ext-tools-start-eclipse
+(defun ext-tools-start-eclipse ()
   "launch eclipse"
   (interactive
    (ext-tools-launch ext-tools-path-ide-java)))
 
-(defun ext-tools-start-browser
+(defun ext-tools-start-browser ()
   "launch browser"
   (interactive
    (ext-tools-launch ext-tools-path-browser)))
 
-(defun ext-tools-start-mail
+(defun ext-tools-start-mail ()
   "launch mail"
   (interactive
    (ext-tools-launch ext-tools-path-mail)))
 
-(defun ext-tools-start-music
+(defun ext-tools-start-music ()
   "launch music"
   (interactive
    (ext-tools-launch ext-tools-path-music)))
 
-(defun ext-tools-start-git-bash
+(defun ext-tools-start-git-bash () 
   "launch git-bash"
   (interactive
    (ext-tools-launch ext-tools-path-gitbash)))
+
+(defun ext-tools-start-sql-server-mgmt ()
+  "launch sql server management studio"
+  (interactive
+   (ext-tools-launch ext-tools-path-sql-server-mgmt)))

948100001abf97244a49efebec4ee88212554165
Tim Hoolihan <Tim@Hoolihan.net>
2011-06-10 11:32:00 -0400

    config updates around files

--- config/config-info.el
+++ config/config-info.el
@@ -1,4 +1,5 @@
 ; info path
-(setq Info-additional-directory-list  
-      '((concat emacs-directory "ecb/info-help/")
-        (concat emacs-directory "nxml/")))
+(setq Info-additional-directory-list  '())
+;      '((filename-as-directory (concat emacs-directory "ecb/info-help/"))
+;        (concat emacs-directory "nxml/")
+;))

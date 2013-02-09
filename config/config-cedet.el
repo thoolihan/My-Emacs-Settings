827f0cc71e5402a9994189cd235ba65ef0c11324
Tim Hoolihan <tim@hoolihan.net>
2013-02-09 01:37:33 -0500

    added .gitignore, changed some config files

--- config/config-cedet.el
+++ config/config-cedet.el
@@ -1,2 +1,6 @@
+(add-path "cedet-1.1/")
+(add-path "cedet-1.1/common/")
+(require 'cedet)
+
 (global-ede-mode 1)
 (semantic-mode 1)
--- config/config-ecb.el
+++ config/config-ecb.el
@@ -10,30 +10,6 @@
 (provide 'semanticdb-mode)
 (provide 'semantic-load)
 
-; semantic
-;(autoload 'semantic "semantic" nil t)
-;(setq semantic-load-turn-everything-on t)
-;(require 'semantic-load)
-
-; speedbar
-(autoload 'speedbar "speedbar" nil t)
-   (define-key-after (lookup-key global-map [menu-bar tools])
-      [speedbar] '("Speedbar" . speedbar-frame-mode) [calendar])
-(global-set-key [(f4)] 'speedbar-get-focus)
-
+(add-path "ecb-2.40/")
 (require 'ecb)
 
-(autoload 'window-number-mode "window-number"
-  "A global minor mode that enables selection of windows according to
-numbers with the C-x C-j prefix.  Another mode,
-`window-number-meta-mode' enables the use of the M- prefix."
-  t)
-
-(autoload 'window-number-meta-mode "window-number"
-  "A global minor mode that enables use of the M- prefix to select
-windows, use `window-number-mode' to display the window numbers in
-the mode-line."
-  t)
-
-(window-number-mode 1)
-(window-number-meta-mode 1)

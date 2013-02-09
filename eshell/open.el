f786237ddb2148f44f66d9e94e0bce2da170ff7f
Tim Hoolihan <tim@hoolihan.net>
2013-02-09 02:15:28 -0500

    removed unused directory

--- eshell/open.el
+++
@@ -1,3 +0,0 @@
-(defun eshell/op (FILE)
-  "Invoke (w32-shell-execute \"Open\" FILE) and substitute slashes for backslashes"
-  (w32-shell-execute "Open" (substitute ?\\ ?/ (expand-file-name FILE))))
